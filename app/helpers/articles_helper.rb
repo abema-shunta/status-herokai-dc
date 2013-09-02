19b50b858cb3481796d7ee6942f18dba4ce330e7
古川 俊太 <shunter@furukawa-shunta-no-iMac.local>
2013-09-02 22:07:39 +0900

    Added tweet button

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,11 @@
 module ArticlesHelper
+  def tweet_text(article)
+    ret = "\
+#{article[:name]}を翻訳しました。 \
+https://github.com/herokaijp/devcenter/wiki/#{article[:url]} \
+https://devcenter.heroku.com/articles/#{article[:url]} \
+#herokai_dc" 
+    puts ERB::Util.url_encode(ret.chomp)
+    ERB::Util.url_encode(ret.chomp)
+  end
 end
